4383fdef87991009198d5bb23f9effd9fb7b61fd
rxl211 <rafael@case.edu>
2017-02-04 21:18:10 -0800

    Made editor slightly nicer looking

--- OneSnip/Editor.cs
+++ OneSnip/Editor.cs
@@ -42,21 +42,80 @@ namespace OneSnip
 
             image = (Bitmap)img;
 
-            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize; //this makes sure the picturebox can fit the whole image (but it has nothing to do with the size of the window)
             pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.Dock = DockStyle.Fill;
+            //bottomPanel.Margin = new Padding(20);
 
-            splitContainer1.MinimumSize = new Size(image.Width + 20, image.Height + splitContainer1.Panel1.Height);
+            //tableLayoutPanel1.MinimumSize = new Size(image.Width + 20, image.Height + topPanel.Height); //this doesn't seem to be doing anything?
 
-            this.SetClientSizeCore(image.Width + 20, image.Height + splitContainer1.Panel1.Height + 20);
+            topPanel.AutoSize = true;
+            topPanel.Width = image.Width;
+
+            bottomPanel.AutoSize = true;
+            bottomPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            //tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, image.Height));
+            tableLayoutPanel1.Controls.Add(this.bottomPanel, 0, 1);
+
+            tableLayoutPanel1.Size = new Size(image.Width, bottomPanel.Height + topPanel.Height);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            //this.SetClientSizeCore(image.Width, image.Height + topPanel.Height - 900); //this is setting the size of the window
+            //this.ClientSize = new Size(image.Width, image.Height + topPanel.Height);
+
+
+            //flowLayoutPanel1.Dock = DockStyle.Fill;
+
+            for (int i = 0; i < flowLayoutPanel1.Controls.Count; i++)
+            {
+                Control b = flowLayoutPanel1.Controls[i];
+
+                if (i < flowLayoutPanel1.Controls.Count - 1)
+                {
+                    b.Margin = new Padding(0, 0, 20, 0);
+                } else
+                {
+                    b.Margin = new Padding(0, 0, 0, 0); //we don't want to add right-margin to the last element
+                }
+            }
+
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.Anchor = AnchorStyles.None;
+            //flowLayoutPanel1.BackColor = Color.Green;
+
+            
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.Dock = DockStyle.Top;
+
+            tableLayoutPanel2.Controls.Add(flowLayoutPanel1);
+            //tableLayoutPanel2.BackColor = Color.Pink;
+            tableLayoutPanel2.Height = flowLayoutPanel1.Height;
+
+            
 
             pictureBox1.Image = image;
-            pictureBox1.Location = new Point((pictureBox1.Parent.ClientSize.Width / 2) - (pictureBox1.Width / 2),
-                                        (pictureBox1.Parent.ClientSize.Height / 2) - (pictureBox1.Height / 2));
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.Location = new Point((bottomPanel.Width / 2) - (pictureBox1.Width / 2),
+                                         (bottomPanel.Height / 2) - (pictureBox1.Height / 2));
+
+            //pictureBox1.Location = new Point(0, 0);
             pictureBox1.Refresh();
 
             pictureBox1.MouseDown += PictureBox1_MouseDown;
             pictureBox1.MouseMove += PictureBox1_MouseMove;
             pictureBox1.MouseUp += PictureBox1_MouseUp;
+
+            Color mainFrame = Color.FromArgb(38, 38, 38);
+            Color toolbar = Color.FromArgb(13, 13, 13);
+
+            tableLayoutPanel2.BackColor = toolbar;
+            bottomPanel.BackColor = mainFrame;
+            tableLayoutPanel1.BackColor = mainFrame;
+
 
             button_Undo.Enabled = false;
 
@@ -64,7 +123,7 @@ namespace OneSnip
 
         private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            
+
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
                 //starts coordinates for rectangle
@@ -86,7 +145,7 @@ namespace OneSnip
 
         private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            if(drawing)
+            if (drawing)
             {
                 Pen pen = new Pen(Color.Red, 4);
 
